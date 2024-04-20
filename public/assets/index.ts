4576f568dd5632bccda5d47b98bc547a3a968779
bishakhne0gi <bneogi102002@gmail.com>
2024-04-21 00:31:38 +0530

    feat: :sparkles: added the initial navbar and v1

---
+++ public/assets/index.ts
@@ -0,0 +1,6 @@
+import Auth from './Auth.svg';
+
+export const Usages =
+{
+    Auth
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import "../styles/globals.css";
+import { Card } from "@/components/ui/card";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/custom/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <ThemeProvider>
+        <body>
+          <Navbar />
+          <Card className='min-h-screen border-none rounded-none flex'>
+            {children}
+          </Card>
+        </body>
+      </ThemeProvider>
     </html>
   );
 }

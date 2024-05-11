7822dadcc4c81dbd4090dce79cb9a2c2e25fd7a2
bishakhne0gi <bneogi102002@gmail.com>
2024-05-12 02:27:06 +0530

    feat: :sparkles: added all new components

--- src/app/orgs/page.tsx
+++ src/app/orgs/page.tsx
@@ -6,6 +6,18 @@ import { Org } from "@/utilities/types";
 import { getOrgDetails } from "@/lib/orgs.query";
 import { getUsers } from "@/lib/users.query";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { JoinedUser } from "@/components/custom/JoinedUser";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Member } from "@/components/custom/Member";
 export default async function Orgs() {
   const data = await getSession();
   if (!data) {
@@ -16,20 +28,46 @@ export default async function Orgs() {
   console.log(data);
 
   return (
-    <div className="flex flex-col p-10 w-full">
-      <div className="flex justify-between">
-        <div className="flex flex-col">
-          <h1 className="text-3xl font-semibold">Hello there {data?.name}!</h1>
-          <p className="text-grey ">
+    <div className='flex flex-col p-10 w-full'>
+      <div className='flex justify-between'>
+        <div className='flex flex-col'>
+          <h1 className='text-3xl font-semibold'>Hello there {data?.name}!</h1>
+          <p className='text-grey '>
             Here&apos;s the list of organisations you have joined
           </p>
         </div>
-        <Avatar>
-          <AvatarImage src={data?.picture} alt="ok" />
-          <AvatarFallback>
-            {data?.name.substr(0, 2).toUpperCase()}
-          </AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
+              <Avatar>
+                <AvatarImage src={data?.picture} alt='ok' />
+                <AvatarFallback>
+                  {data?.name.substr(0, 2).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className='w-56' align='end' forceMount>
+            <DropdownMenuLabel className='font-normal'>
+              <div className='flex flex-col space-y-1'>
+                <p className='text-sm font-medium leading-none'>{data?.name}</p>
+                <p className='text-xs leading-none text-muted-foreground'>
+                  {data?.email}
+                </p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Log out</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className='flex gap-4 w-full'>
+        <div className='flex basis-[50%] w-full'>
+          <JoinedUser />
+        </div>
+        <div className='flex flex-col basis-[50%] w-full'>
+          <Member />
+        </div>
       </div>
       <OrganisationTable data={orgDetails as Org[]} />
     </div>

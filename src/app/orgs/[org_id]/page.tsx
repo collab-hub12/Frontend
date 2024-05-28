6bdbe3a02e66ec8647b1797731e1730405611a2f
bishakhne0gi <bneogi102002@gmail.com>
2024-05-28 14:53:08 +0530

    feat: :sparkles: loader added

--- src/app/orgs/[org_id]/page.tsx
+++ src/app/orgs/[org_id]/page.tsx
@@ -31,8 +31,8 @@ export default async function Teams({
 
   if (orgDetailResponse?.error === "Forbidden") {
     return (
-      <div className="flex m-10 w-full my-auto ">
-        <h1 className="text-2xl text-center font-semibold">
+      <div className='flex m-10 w-full my-auto '>
+        <h1 className='text-2xl text-center font-semibold'>
           User is not part of this org
         </h1>
       </div>
@@ -42,23 +42,23 @@ export default async function Teams({
   // console.log(memberDetailsOfOrg);
   // const teamDetails = await getTeamDetails();
   return (
-    <div className="flex flex-col p-10 w-full">
-      <Toaster position="bottom-left" reverseOrder={false} />
-      <div className="flex justify-between bg-[#b55bb0] px-10 py-4 rounded-md shadow-2xl drop-shadow-xl w-full">
-        <div className="flex justify-between w-full">
-          <h1 className="text-3xl font-semibold dark:text-white text-white ">
+    <div className='flex flex-col p-10 w-full'>
+      <Toaster position='bottom-left' reverseOrder={false} />
+      <div className='flex justify-between bg-[#1967D2] px-10 py-4 rounded-md shadow-2xl drop-shadow-xl w-full'>
+        <div className='flex justify-between w-full'>
+          <h1 className='text-3xl font-semibold dark:text-white text-white '>
             {(orgDetailResponse as Org)?.org_name}
           </h1>
-          <div className="flex gap-2">
-            <div className="px-6 rounded-md flex gap-2 items-center bg-white">
-              <Building color="#b55bb0" />
-              <h1 className="text-base  dark:text-[#b55bb0] text-[#b55bb0]">
+          <div className='flex gap-2'>
+            <div className='px-6 rounded-md flex gap-2 items-center bg-white'>
+              <Building color='#1967D2' />
+              <h1 className='text-base  dark:text-[#1967D2] text-[#1967D2]'>
                 {(orgDetailResponse as Org)?.org_desc}
               </h1>
             </div>
-            <div className="px-6 rounded-md flex gap-2 items-center bg-white">
-              <MapPin color="#b55bb0" />
-              <h1 className="text-base  dark:text-[#b55bb0] text-[#b55bb0]">
+            <div className='px-6 rounded-md flex gap-2 items-center bg-white'>
+              <MapPin color='#1967D2' />
+              <h1 className='text-base  dark:text-[#1967D2] text-[#1967D2]'>
                 {(orgDetailResponse as Org)?.location}
               </h1>
             </div>
@@ -69,11 +69,11 @@ export default async function Teams({
         data={teamDetailsResponse as Team[]}
         org_id={params.org_id!}
       />
-      <div className="flex gap-4 w-full">
-        <div className="flex basis-[50%] w-full">
+      <div className='flex gap-4 w-full'>
+        <div className='flex basis-[50%] w-full'>
           <JoinedUser data={memberDetailsOfOrg as User[]} />
         </div>
-        <div className="flex flex-col basis-[50%] w-full">
+        <div className='flex flex-col basis-[50%] w-full'>
           <Member />
         </div>
       </div>
--- src/app/orgs/[org_id]/teams/[team_name]/tasks/[task_id]/page.tsx
+++ src/app/orgs/[org_id]/teams/[team_name]/tasks/[task_id]/page.tsx
@@ -12,25 +12,25 @@ const page = async ({
   const taskDetail = (await getTaskDetails(org_id, team_name, task_id)) as Task;
 
   return (
-    <div className="flex flex-col p-10 w-full gap-6">
+    <div className='flex flex-col p-10 w-full gap-6'>
       {/* Task Title */}
-      <Toaster position="bottom-left" reverseOrder={false} />
+      <Toaster position='bottom-left' reverseOrder={false} />
 
-      <div className="flex flex-col gap-4">
-        <div className="font-semibold text-[28px]">{taskDetail.title}</div>
-        <div className="flex gap-4 items-center ">
-          <div className="bg-[#EA4335] rounded-full w-[10px] h-[10px]"></div>
-          <div className="text-[#b55bb0] font-medium text-[20px]">
+      <div className='flex flex-col gap-4'>
+        <div className='font-semibold text-[28px]'>{taskDetail.title}</div>
+        <div className='flex gap-4 items-center '>
+          <div className='bg-[#EA4335] rounded-full w-[10px] h-[10px]'></div>
+          <div className='text-[#1967D2] font-medium text-[20px]'>
             {taskDetail.task_progress}
           </div>
-          <span className="p-5 ml-auto">Assigned to :</span>
-          <div className="flex -space-x-4 rtl:space-x-reverse">
+          <span className='p-5 ml-auto'>Assigned to :</span>
+          <div className='flex -space-x-4 rtl:space-x-reverse'>
             {taskDetail.assigned_to?.map((user) => (
               <img
                 key={user.id}
                 width={40}
                 height={40}
-                className="border-2 border-white rounded-full dark:border-gray-800"
+                className='border-2 border-white rounded-full dark:border-gray-800'
                 src={user.picture}
                 alt={user.name}
               />
@@ -38,33 +38,33 @@ const page = async ({
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-4 px-6">
-        <h1 className="font-bold text-[20px]">DETAILS</h1>
-        <div className="flex gap-8">
-          <div className="flex gap-4 items-center">
-            <div className="h-[50px] w-[60px] bg-[#b55bb0] rounded-md"></div>
-            <div className="flex flex-col gap-1">
-              <p className="text-[12px] text-[#9C9C9D]">Task Manager</p>
+      <div className='flex flex-col gap-4 px-6'>
+        <h1 className='font-bold text-[20px]'>DETAILS</h1>
+        <div className='flex gap-8'>
+          <div className='flex gap-4 items-center'>
+            <div className='h-[50px] w-[60px] bg-[#1967D2] rounded-md'></div>
+            <div className='flex flex-col gap-1'>
+              <p className='text-[12px] text-[#9C9C9D]'>Task Manager</p>
               {/* Name of the task assigner */}
-              <p className="text-[20px] font-bold">Bishakh Neogi</p>
+              <p className='text-[20px] font-bold'>Bishakh Neogi</p>
             </div>
           </div>
-          <div className="flex gap-4 items-center">
-            <div className="h-[50px] w-[60px] bg-[#b55bb0] rounded-md"></div>
-            <div className="flex flex-col gap-1">
-              <p className="text-[12px] text-[#9C9C9D]">Task Deadline</p>
+          <div className='flex gap-4 items-center'>
+            <div className='h-[50px] w-[60px] bg-[#1967D2] rounded-md'></div>
+            <div className='flex flex-col gap-1'>
+              <p className='text-[12px] text-[#9C9C9D]'>Task Deadline</p>
               {/* Deadline of the task */}
-              <p className="text-[20px] font-bold">
+              <p className='text-[20px] font-bold'>
                 {taskDetail.task_deadline}
               </p>
             </div>
           </div>
         </div>
       </div>
-      <div className="rounded-md border-dotted dark:border-slate-800 border-2 p-6 w-[60%] gap-4 flex flex-col">
-        <h1 className="font-bold text-[20px]">DESCRIPTION</h1>
+      <div className='rounded-md border-dotted dark:border-slate-800 border-2 p-6 w-[60%] gap-4 flex flex-col'>
+        <h1 className='font-bold text-[20px]'>DESCRIPTION</h1>
         {/* Description of the task */}
-        <h1 className="text-[14px] text-grey">{taskDetail.task_desc}</h1>
+        <h1 className='text-[14px] text-grey'>{taskDetail.task_desc}</h1>
       </div>
 
       <Member org_id={params.org_id} team_name={params.team_name} />
